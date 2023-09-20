f52b0412cc12ee45ff1d2ebd92dcc6716e9dffec
Bharath Vedartham <vedabharath12345@gmail.com>
2023-09-20 18:35:08 +0530

    replace the EC url

--- constants/addresses.go
+++ constants/addresses.go
@@ -8,5 +8,5 @@ var (
 	WethAddress     = common.HexToAddress("0x4c849Ff66a6F0A954cbf7818b8a763105C2787D6")
 	AtomicSwap      = common.HexToAddress("0xB9D7a3554F221B34f49d7d3C61375E603aFb699e")
 	UniswapFactoryA = common.HexToAddress("0xBFF5cD0aA560e1d1C6B1E2C347860aDAe1bd8235")
-	EcUrl           = "13.127.243.252:35347"
+	EcUrl           = "<your EC i.p address>:<EC port>"
 )
