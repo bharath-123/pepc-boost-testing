5fb6058bbc6f23c99bed1a423ccf2bc3df98f810
Bharath Vedartham <vedabharath12345@gmail.com>
2023-09-28 01:10:41 +0530

    add some documentation

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-func TransferWeth() {
+func TransferAndApproveWeth() {
 	ecRpcUrl := fmt.Sprintf("http://%s", constants.BuilderUrl)
 	fmt.Println("1. Creating EC client")
 	client, err := ethclient.Dial(ecRpcUrl)
@@ -86,8 +86,12 @@ func TransferWeth() {
 }
 
 func main() {
-	TransferWeth()
+	// Transfer Weth and approve its spending to the AtomicSwap contract
+	TransferAndApproveWeth()
 
+	/*
+		Prepare the information required to create a ToB tx request
+	*/
 	fmt.Printf("Fetching the current slot from the relayer\n")
 	currentSlot, err := util.GetCurrentSlot(fmt.Sprintf("http://%s/eth/v1/relay/get_head_slot", constants.MevRelayerUrl))
 	if err != nil {
@@ -112,33 +116,31 @@ func main() {
 	}
 	fmt.Println("proposer fee recipient: ", proposerFeeRecipient)
 
-	fmt.Println("2. Creating private key")
+	/*
+		Prepare the sender
+	*/
 	privateKey, err := crypto.HexToECDSA("df9bb6de5d3dc59595bcaa676397d837ff49441d211878c024eabda2cd067c9f")
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
 
-	fmt.Println("3. Creating sender instance")
 	// Create a new instance of the Ethereum sender's wallet
 	sender := crypto.PubkeyToAddress(privateKey.PublicKey)
 
+	/*
+		Prepare the EC client
+	*/
 	ecRpcUrl := fmt.Sprintf("http://%s", constants.BuilderUrl)
-	fmt.Println("1. Creating EC client")
 	client, err := ethclient.Dial(ecRpcUrl)
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-	fmt.Printf("client: %v\n", client)
 
-	senderBalance, err := util.GetBalance(client, sender.String())
-	if err != nil {
-		fmt.Printf("Error fetching balance for sender: %v", err)
-		return
-	}
-	fmt.Printf("Sender balance: %v\n", senderBalance)
-
+	/*
+		Prepare the atomic swap
+	*/
 	chainId, err := client.ChainID(context.Background())
 	if err != nil {
 		log.Fatal(err)
@@ -167,16 +169,15 @@ func main() {
 	transactor.NoSend = true
 	transactor.Nonce = big.NewInt(int64(nonce))
 
-	fmt.Printf("Creating swap tx\n")
 	swapTx, err := atomicSwapContract.Swap(transactor, []common.Address{constants.WethAddress, constants.DaiAddress}, big.NewInt(100000000000000000), constants.UniswapFactoryA, sender, false)
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-	fmt.Printf("Created tx!!\n", swapTx.Hash())
 
-	// Replace with the recipient's address
-	fmt.Println("5. Creating relayer instance")
+	/*
+		Prepare the proposer payout
+	*/
 	proposerFeeRecipientAddress := common.HexToAddress(proposerFeeRecipient)
 
 	proposerFeeRecipientBalance, err := util.GetBalance(client, proposerFeeRecipientAddress.String())
@@ -186,37 +187,23 @@ func main() {
 	}
 	fmt.Printf("proposer fee recipient address balance: %v\n", proposerFeeRecipientBalance)
 
-	fmt.Printf("Nonce: %d\n", nonce)
-	fmt.Printf("Swap tx nonce is: %d\n", swapTx.Nonce())
-
-	// Set the gas price and gas limit
-	fmt.Println("7. Setting gas price and gas limit")
 	gasPrice, err := client.SuggestGasPrice(context.Background())
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
 
-	gasLimit := uint64(21000) // You may need to adjust this depending on the type of transaction
+	gasLimit := uint64(21000)
 
-	// Specify the amount to send (in Wei)
-	value := big.NewInt(1000000000000000000) // 0.1 ETH
+	value := big.NewInt(1000000000000000000) // 1 ETH
 
 	// Create a new Ethereum transaction
-	fmt.Printf("8. Creating the txs")
-	//tobTx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, nil)
 	proposerPayout := types.NewTransaction(swapTx.Nonce()+1, proposerFeeRecipientAddress, value, gasLimit, gasPrice, nil)
 
-	fmt.Println("9. get local chain id")
-	localChainId, err := client.ChainID(context.Background())
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	fmt.Printf("ChainID: %d\n", localChainId)
-
-	// Sign the transaction with the sender's private key
-	signedProposerPayout, err := types.SignTx(proposerPayout, types.NewCancunSigner(localChainId), privateKey)
+	/*
+		Sign the ToB txs
+	*/
+	signedProposerPayout, err := types.SignTx(proposerPayout, types.NewCancunSigner(chainId), privateKey)
 	if err != nil {
 		log.Fatal(err)
 		return
@@ -232,6 +219,9 @@ func main() {
 		return
 	}
 
+	/*
+		Prepare the ToB tx request to the PEPC-Boost relay
+	*/
 	tobTxs := new(types2.TobTxsSubmitRequest)
 	tobTxs.TobTxs = utilbellatrix.ExecutionPayloadTransactions{
 		Transactions: []bellatrix.Transaction{binaryTx1, binaryTx2},
@@ -244,7 +234,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println("10. Sending transactions to mev relayer")
+	/*
+		Send the ToB tx request to the PEPC-Boost relay
+	*/
 	// Create a new HTTP POST request with the JSON data as the request body
 	req, err := http.NewRequest("POST", fmt.Sprintf("http://%s/relay/v1/builder/tob_txs", constants.MevRelayerUrl), bytes.NewBuffer(jsonTobTxs))
 	if err != nil {
@@ -263,6 +255,9 @@ func main() {
 	}
 	defer resp.Body.Close()
 
+	/*
+		Verify the output
+	*/
 	// Check the response status code
 	if resp.StatusCode == http.StatusOK {
 		fmt.Printf("Tob Txs successfully submitted for slot: %d and parentHash: %s\n", tobTxs.Slot, tobTxs.ParentHash)
@@ -275,7 +270,7 @@ func main() {
 	}
 
 	//======
-	//Block inspection
+	//Block inspection, use the below code to inspect your block hash txs to check if your expected txs are built on the top of the block
 	//======
 	//ecRpcUrl := fmt.Sprintf("http://%s", constants.BuilderUrl)
 	//fmt.Println("1. Creating EC client")
@@ -286,7 +281,7 @@ func main() {
 	//}
 	//fmt.Printf("client: %v\n", client)
 	//
-	//blockDetails, err := util.GetBlockByBlockHash(client, "0x75cd835787486af61df75d96fe82fba7a2c4a47d03cc822b02171e6152d7fd8c")
+	//blockDetails, err := util.GetBlockByBlockHash(client, "0x3008220e24de309adf019b5c11c8317d3972aa2ab77414e20ba141dc7093f743")
 	//if err != nil {
 	//	fmt.Printf("Error fetching block by hash: %v", err)
 	//	return
@@ -308,9 +303,6 @@ func main() {
 	//		return
 	//	}
 	//	fmt.Printf("Tx status is %d\n", receipt.Status)
-	//	//receipt.
-	//	//fmt.Printf("Tx status is %d\n", receipt.Status)
-	//	//receipt.
 	//	i++
 	//	if i > 2 {
 	//		break
@@ -318,29 +310,3 @@ func main() {
 	//}
 
 }
-
-//res, err := util.GetCallTraces(swapTx)
-//if err != nil {
-//	log.Fatal(err)
-//	return
-//}
-//
-//fmt.Printf("Res is %v\n", res.Result)
-
-//
-//atomicSwapMetadata, err := contracts.AtomicSwapMetaData.GetAbi()
-//if err != nil {
-//	log.Fatal(err)
-//	return
-//}
-//swapId := atomicSwapMetadata.Methods["swap"].ID
-//fmt.Printf("id: %x\n", swapId)
-//
-//res := atomicSwapMetadata.Methods["swap"]
-//argsMap := make(map[string]interface{})
-//err = res.Inputs.UnpackIntoMap(argsMap, swapTx.Data())
-//if err != nil {
-//	log.Fatal(err)
-//	return
-//}
-//fmt.Printf("res is %v\n", argsMap)
