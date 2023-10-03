f41d2eb08f5eb3acc80654e5631070f605c1859d
Bharath Vedartham <vedabharath12345@gmail.com>
2023-10-03 12:50:57 +0530

    remove builder urls

--- constants/addresses.go
+++ constants/addresses.go
@@ -1,20 +1,5 @@
 package constants
 
-import "github.com/ethereum/go-ethereum/common"
-
 var (
-	// addresses from kurtosis devnet
-	// addresses from kurtosis devnet
-	DaiAddress        = common.HexToAddress("0xAb2A01BC351770D09611Ac80f1DE076D56E0487d")
-	WethAddress       = common.HexToAddress("0x4c849Ff66a6F0A954cbf7818b8a763105C2787D6")
-	WethGoerliAddress = common.HexToAddress("0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6")
-	AtomicSwap        = common.HexToAddress("0xB9D7a3554F221B34f49d7d3C61375E603aFb699e")
-	UniswapFactoryA   = common.HexToAddress("0xBFF5cD0aA560e1d1C6B1E2C347860aDAe1bd8235")
-	UniV3SwapRouter   = common.HexToAddress("0xE592427A0AEce92De3Edee1F18E0157C05861564")
-	UniV3QuoterV2     = common.HexToAddress("0x61fFE014bA17989E743c5F6cB21bF9697530B21e")
-	UniV3Quoter       = common.HexToAddress("0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6")
-	UsdcAddress       = common.HexToAddress("0x9B2660A7BEcd0Bf3d90401D1C214d2CD36317da5")
-	EcUrl             = "<Your EC Url>"
-	MevRelayerUrl     = "172.31.46.248:9062"
-	BuilderUrl        = "65.0.7.230:8545"
+	BuilderUrl = "<builder url>"
 )
